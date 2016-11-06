6608f46d2ffa7781bab32a6da095621089d022a9
Vilayvann Ky <vilayvannky@Vilayvanns-MacBook-Pro.local>
2016-11-06 11:49:06 -0500

    Added contentDownloadSelect.py

--- wsgi/contentDownloadSelect.py
+++ wsgi/contentDownloadSelect.py
@@ -1,11 +1,37 @@
 #! /usr/bin/env python
 import owncloud
+import json
 
-c = owncloud.Client('http://54.224.18.48/nextcloud/')
-c.login('admin', 'biblebox')
-files = []
-for i in c.list('/BibleBox'):
-    files.append(str(i.path).split('/')[-2])
+mimes = {
+	'application/vnd.android.package-archive': 'software'
+}
 
-for i in files:
-    print i
+def ownCloudList(client_url, username, password, root):
+	json_data = {}
+	files = {}
+
+	c = owncloud.Client(client_url)
+	c.login(username, password)
+
+	for file_info in c.list(root, depth='infinity'):
+		if not file_info.is_dir():
+			mimetype = file_info.get_content_type()
+			file_type = mimetype.split('/')[0]
+			if file_type not in ['audio', 'video', 'image']:
+				file_type = mimes.get(mimetype)
+				if file_type is None:
+					file_type = 'text'
+
+			f = files.setdefault(file_type, [])
+			f.append((file_info.name, file_info.path, mimetype))
+			
+	json_data['root'] = [files]
+
+	#with open('filenames.json', 'w') as json_file:
+	print json.dumps(json_data, sort_keys=True,
+	                  indent=4, separators=(',', ': '))
+
+
+if __name__ == '__main__':
+	import sys
+	ownCloudList(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
